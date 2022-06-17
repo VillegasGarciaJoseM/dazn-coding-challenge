5d3a21576d6f7e4a5eec01cd26fac2a9dc7a9e6f
josvilgar1 <josvilgar1@alum.us.es>
2022-06-17 12:55:52 +0200

    Added devices management functionality
    Added endpoints, services and new dependencies

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 const express = require('express');
 const axios = require('axios')
-
 const app = express();
+var bodyParser = require('body-parser')
+const userService = require('./components/devices/userService.js')
+
+var jsonParser = bodyParser.json()
 
 app.get('/devices', (req, res) => {
-    res.status(200).json([{message:'Device retrieve endpoint'}]);
+    userService.findAllDevicesFromUser(req.query.userId)
+        .then(({ code, devices, message }) => {
+            if (code != 200) {
+                res.status(code).json(message)
+            }
+            res.status(code).json(devices)
+        })
+        .catch(err => console.log(err))
 });
 
-app.post('/device/create', (req,res) => {
-    res.status(200).send('Device create endpoint')
+app.post('/device/addDevice', jsonParser, (req, res) => {
+    userService.addDevice(req.body.user, req.body.device)
+        .then(({ code, message }) => {
+            res.status(code).json(message)
+        })
+        .catch(({ message }) => {
+            res.status(500).json(message)
+        });
 })
 
-app.post('/device/update', (req,res) => {
-    res.status(200).send('Device update endpoint')
+app.post('/device/updateDevice', jsonParser, (req, res) => {
+    userService.updateDevice(req.body.user, req.body.device, req.body.activate)
+        .then(({ code, message }) => {
+            res.status(code).json(message)
+        })
+        .catch(({ code, message }) => {
+            res.status(code).json(message)
+        });
 })
 
-app.post('/device/delete', (req,res) => {
-    res.status(200).send('Device delete endpoint')
+app.post('/device/delete', jsonParser, (req, res) => {
+    userService.deleteDevice(req.body.user, req.body.device)
+        .then(({ code, message }) => {
+            res.status(code).json(message)
+        })
+        .catch(({message}) => {
+            res.status(500).json(message)
+        })
 })
 
-app.all('*',(req,res) => {
+app.all('*', (req, res) => {
     res.status(404).send("Page not found!")
 })
 
---
+++ components/externalAPI/reportAPI.js
@@ -0,0 +1,7 @@
+const reportAPI = module.exports;
+
+reportAPI.reportExcessPayableDevicesCase = (user) => {
+    // Call to some external API to report suspicious or fraud system.
+    console.log("Excess number of payable devices case reported")
+    //TODO: log!
+}
